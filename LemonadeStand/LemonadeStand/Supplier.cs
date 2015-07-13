642c4573ca3bee892af8baece394cb3b4c2e220b
mkohlmann-he <mkohlmann-he@dev.bse.com>
2015-07-13 12:54:13 -0500

    Added Getter Setter for inventory

--- LemonadeStand/LemonadeStand/Supplier.cs
+++ LemonadeStand/LemonadeStand/Supplier.cs
@@ -9,14 +9,19 @@ namespace LemonadeStand
 {
     public class Supplier : IUpdate 
     {
-        protected Inventory inventory;
+        protected Inventory lemonInventory;
+        protected Inventory sugarInventory;
+        protected Inventory iceInventory;
+        protected Inventory cupInventory;
         protected float cashOnHand;
-        protected GeoCoordinate Location;
+        protected GeoCoordinate location;
 
 
+        
 
 
-
+        
+        
         public GeoCoordinate Location
         {
             get
@@ -25,9 +30,57 @@ namespace LemonadeStand
             }
             set
             {
-                this.location = ValueType;
+                this.location = value;
             }
         }
+
+        public void recieveShipment(Shipment shipment)
+        {
+
+        }
+
+
+        //
+        // GET INVENTORY METHODS
+        //
+        public List<Ingredient> getLemonInventory()
+        {
+            return this.lemonInventory.getInventory();
+        }
+        public List<Ingredient> getSugarInventory()
+        {
+            return this.sugarInventory.getInventory();
+        }
+        public List<Ingredient> getIceInventory()
+        {
+            return this.iceInventory.getInventory();
+        }
+        public List<Ingredient> getCupInventory()
+        {
+            return this.cupInventory.getInventory();
+        }
+
+
+        //
+        // SET INVENTORY METHODS
+        //
+        public void setLemonInventory(List<Ingredient> inventory)
+        {
+            this.lemonInventory.setInventory(inventory);
+        }
+        public void setSugarInventory(List<Ingredient> inventory)
+        {
+            this.sugarInventory.setInventory(inventory);
+        }
+        public void setIceInventory(List<Ingredient> inventory)
+        {
+            this.iceInventory.setInventory(inventory);
+        }
+        public void setCupInventory(List<Ingredient> inventory)
+        {
+            this.cupInventory.setInventory(inventory);
+        }
+
 
 
 
@@ -35,8 +88,11 @@ namespace LemonadeStand
         {
             inventory.update();
 
+            this.cashOnHand = this.calculateOperatingCost();
         }
 
+
+        protected float calculateOperatingCost();
 
 
     }
