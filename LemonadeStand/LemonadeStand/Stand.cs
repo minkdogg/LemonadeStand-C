8f4dc709b9c051700f5db109b5026b862eb50ae3
rroethle <rroethle_he@dev.bse.edu>
2015-07-13 12:27:22 -0500

    Added Stand class

---
+++ LemonadeStand/LemonadeStand/Stand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Stand
+    {
+           
+        public float cash;
+        public int lemons;
+        public int ice;
+        public int sugar;
+        public string location;
+
+          public Stand(string standLocation)
+        {
+            this.cash = 500;
+            this.lemons = 0;
+            this.sugar = 0;
+            this.ice = 0;
+            this.location = standLocation;
+        }
+
+    
+}
+    
+}
