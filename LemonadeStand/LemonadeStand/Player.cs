fc36ae285525a5787122ac71d9f1140a2a275a05
Matthew Runkle <mrunkle_he@dev.bse.edu>
2015-07-13 12:49:59 -0500

    player class

---
+++ LemonadeStand/LemonadeStand/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    public class Player
+    {
+        protected string playerName;
+        protected int hiScore;
+
+        public Player(string name)
+        {
+            Stand name = new Stand();
+            this.playerName = name;
+        }
+
+        public int PlayerName
+        {
+            get
+            {
+                return playerName;
+            }
+        }
+
+        public int HiScore
+        {
+            get
+            {
+                return hiScore;
+            }
+        }
+    }
+}
