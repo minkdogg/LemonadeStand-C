486a3a9eaab3cfe0d722e41e0ed8e53c045f12df
mkohlmann-he <mkohlmann-he@dev.bse.com>
2015-07-13 10:42:51 -0500

    Initial Stub of Supplier.cs

---
+++ LemonadeStand/LemonadeStand/Supplier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Supplier
+    {
+        protected Inventory inventory;
+        protected float cashOnHand;
+        protected float location;
+
+
+
+
+
+
+
+
+
+    }
+}
