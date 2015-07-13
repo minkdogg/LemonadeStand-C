09ccfc5514239cb2ba97c038b7477540ed1b06cd
mkohlmann-he <mkohlmann-he@dev.bse.com>
2015-07-13 11:48:32 -0500

    Pull Update

--- LemonadeStand/LemonadeStand/Supplier.cs
+++ LemonadeStand/LemonadeStand/Supplier.cs
@@ -3,20 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Device.Location;
 
 namespace LemonadeStand
 {
-    class Supplier
+    public class Supplier : IUpdate 
     {
         protected Inventory inventory;
         protected float cashOnHand;
-        protected float location;
+        protected GeoCoordinate Location;
 
 
 
 
 
+        public GeoCoordinate Location
+        {
+            get
+            {
+                return this.location;
+            }
+            set
+            {
+                this.location = ValueType;
+            }
+        }
 
+
+
+        public override void update()
+        {
+            inventory.update();
+
+        }
 
 
 
