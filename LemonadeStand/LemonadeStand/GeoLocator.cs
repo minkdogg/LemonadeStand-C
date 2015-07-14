50d9b1757918a9aa332e4e969e78ac3a19121140
mkohlmann-he <mkohlmann-he@dev.bse.com>
2015-07-14 08:58:58 -0500

    Added Geolocator

---
+++ LemonadeStand/LemonadeStand/GeoLocator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Xml.Linq;
+
+namespace TempWorkingProject
+{
+    class GeoLocator
+    {
+
+        public string getLocation(string address)
+        {
+            var requestUri = string.Format("http://maps.googleapis.com/maps/api/geocode/xml?address={0}&sensor=false", Uri.EscapeDataString(address));
+
+            var request = WebRequest.Create(requestUri);
+            var response = request.GetResponse();
+            var xdoc = XDocument.Load(response.GetResponseStream());
+
+            var result = xdoc.Element("GeocodeResponse").Element("result");
+            var locationElement = result.Element("geometry").Element("location");
+            string lat = locationElement.Element("lat").Value;
+            string lng = locationElement.Element("lng").Value;
+
+            return lat;
+        }
+    }
+}
